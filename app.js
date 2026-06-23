b52eecb26153a43eb7d4b261f943546a4e89e33a
Raj Gupta <rajgupta807633@gmail.com>
2026-06-23 18:47:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.set("view engine", "ejs");
+app.use (express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+const fs = require("fs");
+
+
+app.get("/", (req, res) => {  
+    res.send("Hello, World!");
+
+});
+
+// creating files with name of current date in format dd-mm-yyyy
+app.get("/create", (req, res) => {
+    const today = new Date();
+
+const day = String(today.getDate()).padStart(2, '0');
+const month = String(today.getMonth() + 1).padStart(2, '0');
+const year = today.getFullYear();
+
+const fn = `${day}-${month}-${year}`;
+
+    fs.writeFile(`./files/${fn}`, "Hello, World!", (err) => {
+        if(err) return res.send("Error creating file");
+        else res.send("File created successfully");
+    })
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
